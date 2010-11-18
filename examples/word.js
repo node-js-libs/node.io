dca08e088f70e21ef38003955c5a3f74be94cdbe
Chris O'Hara <cohara87@gmail.com>
2010-11-18 22:00:13 +1000

    Fixed stream.paused being removed from node + version bump

--- examples/word.js
+++
@@ -1,8 +0,0 @@
-var start = require('../').start;
-
-var wc = require('./word_count').job.extend({benchmark:true,fork:2}, {input:'./lorem.txt'});
-
-start(wc, function(err, output) {
-    //console.log(err);
-    //console.log(output);
-});
