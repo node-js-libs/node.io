83340b1812c28db7118bf3853ee69eb48e1a8936
Chris O'Hara <cohara87@gmail.com>
2010-12-15 14:45:40 +1000

    Fixed up spell example

--- examples/google_spell.js
+++ examples/google_spell.js
@@ -4,7 +4,7 @@
 //       $ echo "definately" | node.io -s google_spell    
 //          => definitely
 
-var Job = require('node.io').Job;
+var Job = require('/cygdrive/c/Users/Chris/My Dropbox/Work/Node/node.io').Job;
 
  //Timeout after 10s, maximum of 3 retries
 var options = {timeout:10, retries:3};
@@ -14,11 +14,13 @@ var methods = {
     run: function google(input) {
         var spell, self = this;
         
-        this.getHtml('http://www.google.com/search?hl=en&q='+encodeURIComponent(input), function(err, $) {
-            if (err) self.retry();
-            
-            if (spell = $('a.spell')) {
-                self.emit(spell.first().fulltext);
+        this.getHtml('http://www.google.com/search?hl=en&q='+encodeURIComponent(input), function(err, $, data) {
+            try {
+                if (err) throw err;
+                spell = $('a.spell')
+                self.emit(spell.first().striptags);
+            } catch (e) {
+                self.retry();
             }
         });
     }, 
